76baca823a8ca6f450d2a40d7f77f2752d4309c9
Destiny Rahymes <Destiny2120@gmail.com>
2023-03-15 13:34:29 -0400

    3/15

---
+++ LearningResourcesSolution/LearningResourcesSolution/LearningResourcesApi/Adapters/LearningResourcesDataContext.cs
@@ -0,0 +1,14 @@
+﻿using LearningResourcesApi.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LearningResourcesApi.Adapters;
+
+public class LearningResourcesDataContext : DbContext
+{
+    public LearningResourcesDataContext(DbContextOptions<LearningResourcesDataContext> options) : base(options)
+    {
+
+    }
+    public DbSet<LearningResourcesEntity> LearningResources { get; set; }
+}
